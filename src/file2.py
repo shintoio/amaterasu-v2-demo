f84911f282d6e6e77449a6d8ace34eec2acd2793
Yaniv Rodenski <roadan@gmail.com>
2017-09-18 09:49:30 +1000

    Update file2.py

--- src/file2.py
+++ src/file2.py
@@ -1,3 +1,3 @@
-high_no_df = ama_context.get_dataframe("start", "odd").where("[number] > 100")
+high_no_df = ama_context.get_dataframe("start", "odd").where("number > 100")
 
 high_no_df.write.save("file:///tmp/test1", format="json")
