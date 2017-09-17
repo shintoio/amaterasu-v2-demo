486cf289181f777a58ff185b9ed49f9236de96fd
Yaniv Rodenski <roadan@gmail.com>
2017-09-18 09:45:04 +1000

    fixed intentention

--- src/file2.py
+++ src/file2.py
@@ -1,5 +1,3 @@
-high_no_df = ama_context
-               .get_dataframe("start", "odd")
-               .where("_1 > 100")
+high_no_df = ama_context.get_dataframe("start", "odd").where("_1 > 100")
 
 high_no_df.write.save("file:///tmp/test1", format="json")
